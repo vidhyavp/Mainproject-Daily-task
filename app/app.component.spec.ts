1bce5313d690a78f8a18dbf7874029bc9280e53e
vidhyavp <47115976+vidhyavp@users.noreply.github.com>
2019-02-01 16:45:17 +0530

    Add files via upload

--- app/app.component.spec.ts
+++ app/app.component.spec.ts
@@ -20,16 +20,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'demoo'`, () => {
+  it(`should have as title 'vidhya'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.debugElement.componentInstance;
-    expect(app.title).toEqual('demoo');
+    expect(app.title).toEqual('vidhya');
   });
 
   it('should render title in a h1 tag', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.debugElement.nativeElement;
-    expect(compiled.querySelector('h1').textContent).toContain('Welcome to demoo!');
+    expect(compiled.querySelector('h1').textContent).toContain('Welcome to vidhya!');
   });
 });
---
+++ app/webform/webform.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-webform',
+  templateUrl: './webform.component.html',
+  styleUrls: ['./webform.component.css']
+})
+export class WebformComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
